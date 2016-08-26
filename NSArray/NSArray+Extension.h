6ed5e5468d8a4c5537d845dcfe65e8767f09b651
Eric <ericliu@hyx.com>
2016-08-26 17:01:07 +0800

    1. same elements between 2 array 2. delete extra elements in array

---
+++ NSArray/NSArray+Extension.h
@@ -0,0 +1,13 @@
+//
+//  NSArray+Extension.h
+//  AccountManager
+//
+//  Created by Eric on 16/8/26.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSArray (Extension)
++ (NSArray *)sameElementsBetweenArrayA:(NSArray *)arrayA arrayB:(NSArray *)arrayB;
+@end
---
+++ NSArray/NSArray+Extension.m
@@ -0,0 +1,48 @@
+//
+//  NSArray+Extension.m
+//  AccountManager
+//
+//  Created by Eric on 16/8/26.
+//  Copyright © 2016年 ericoda. All rights reserved.
+//
+
+#import "NSArray+Extension.h"
+
+@implementation NSArray (Extension)
++ (NSArray *)sameElementsBetweenArrayA:(NSArray *)arrayA arrayB:(NSArray *)arrayB {
+    if (!arrayA || !arrayB || !(arrayA.count != 0) || !(arrayB != 0)) {
+        NSLog(@"error!!\nfunction:%s\nline:%d",__FUNCTION__,__LINE__);
+        return nil;
+    }
+    
+    arrayA = [arrayA filtSameElements];
+    arrayB = [arrayB filtSameElements];
+    
+    NSMutableArray *result = [NSMutableArray new];
+    
+    for (id obj in arrayA) {
+        if ([arrayB containsObject:obj]) {
+            [result addObject:obj];
+        }
+    }
+    
+    return result;
+}
+
+- (NSArray *)filtSameElements {
+    if (self == nil || self.count == 0) {
+        NSLog(@"error!!\nfunction:%s\nline:%d",__FUNCTION__,__LINE__);
+        return nil;
+    }
+    NSMutableArray *result = [NSMutableArray new];
+    NSMutableArray *tmpArr = self.mutableCopy;
+    
+    do {
+        [result addObject:tmpArr[0]];
+        [tmpArr removeObject:tmpArr[0]];
+    } while (tmpArr.count > 0);
+    
+    return result;
+}
+
+@end
